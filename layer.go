a3fe4d27ecbf5db3e1c1dfdbd02e8b12bd68188e
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-07-10 07:01:34 +0200

    Add Layer implementing raft transport layer interface via HTTP

---
+++ layer.go
@@ -0,0 +1,174 @@
+package rafthttp
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/dqlite/raft-membership"
+	"github.com/pkg/errors"
+)
+
+// NewLayer returns a new raft stream layer that initiates connections
+// with HTTP and then uses Upgrade to switch them into raw TCP.
+func NewLayer(path string, localAddr net.Addr, handler *Handler, dial Dial) *Layer {
+	return &Layer{
+		path:      path,
+		localAddr: localAddr,
+		handler:   handler,
+		dial:      dial,
+	}
+}
+
+// Layer represents the connection between raft nodes.
+type Layer struct {
+	path      string
+	localAddr net.Addr
+	handler   *Handler
+	dial      Dial
+}
+
+// Accept waits for the next connection.
+func (l *Layer) Accept() (net.Conn, error) {
+	conn, more := <-l.handler.Connections()
+	if !more {
+		return nil, io.EOF
+	}
+	return conn, nil
+}
+
+// Close closes the layer.
+func (l *Layer) Close() error {
+	l.handler.Close()
+	return nil
+}
+
+// Addr returns the local address for the layer.
+func (l *Layer) Addr() net.Addr {
+	return l.localAddr
+}
+
+// Dial creates a new network connection.
+func (l *Layer) Dial(addr string, timeout time.Duration) (net.Conn, error) {
+	url := l.url()
+	request := &http.Request{
+		Method:     "GET",
+		URL:        url,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     make(http.Header),
+		Host:       url.Host,
+	}
+	request.Header.Set("Upgrade", "raft")
+
+	conn, err := l.dial(addr, timeout)
+	if err != nil {
+		return nil, errors.Wrap(err, "dialing failed")
+	}
+
+	if err := request.Write(conn); err != nil {
+		return nil, errors.Wrap(err, "sending HTTP request failed")
+	}
+
+	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read response")
+	}
+	if response.StatusCode != http.StatusSwitchingProtocols {
+		return nil, fmt.Errorf("dialing fail: expected status code 101 got %d", response.StatusCode)
+	}
+	if response.Header.Get("Upgrade") != "raft" {
+		return nil, fmt.Errorf("missing or unexpected Upgrade header in response")
+	}
+	return conn, err
+}
+
+// Join tries to join the cluster by contacting the leader at the
+// given address.
+func (l *Layer) Join(addr string, timeout time.Duration) error {
+	return l.requestMembershipChange(raftmembership.JoinRequest, addr, timeout)
+}
+
+// Leave tries to leave the cluster by contacting the leader at the
+// given address.
+func (l *Layer) Leave(addr string, timeout time.Duration) error {
+	return l.requestMembershipChange(raftmembership.LeaveRequest, addr, timeout)
+}
+
+// Build a full url.URL object out of our path.
+func (l *Layer) url() *url.URL {
+	url, err := url.Parse(l.path)
+	if err != nil {
+		panic(fmt.Sprintf("invalid URL path %s", l.path))
+	}
+	return url
+}
+
+// Change the membership of the peer associated with this layer.
+func (l *Layer) requestMembershipChange(kind raftmembership.ChangeRequestKind, addr string, timeout time.Duration) error {
+	url := l.url()
+	url.RawQuery = fmt.Sprintf("peer=%s", l.Addr().String())
+	url.Host = addr
+	url.Scheme = "http"
+	method := membershipChangeRequestKindToMethod[kind]
+	request := &http.Request{
+		Method:     method,
+		URL:        url,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     make(http.Header),
+	}
+
+	remaining := timeout
+	var response *http.Response
+	var err error
+	for remaining > 0 {
+		start := time.Now()
+		client := &http.Client{Timeout: remaining}
+		response, err = httpClientDo(client, request)
+
+		// If we got a system or network error, just return it.
+		if err != nil {
+			break
+		}
+
+		// If we got an HTTP error, let's capture its details,
+		// and possibly return it if it's not retriable or we
+		// have hit our timeout.
+		if response.StatusCode != http.StatusOK {
+			body, _ := ioutil.ReadAll(response.Body)
+			err = fmt.Errorf(
+				"http code %d '%s'", response.StatusCode,
+				strings.TrimSpace(string(body)))
+		}
+		// If there's a temporary failure, let's retry.
+		if response.StatusCode == http.StatusServiceUnavailable {
+			// XXX TODO: use an exponential backoff
+			// relative to the timeout?
+			time.Sleep(100 * time.Millisecond)
+
+			remaining -= time.Since(start)
+			continue
+		}
+
+		break
+	}
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("peer %s failed", kind))
+	}
+	return nil
+}
+
+// Map a membership ChangeRequest kind code to an HTTP method name.
+var membershipChangeRequestKindToMethod = map[raftmembership.ChangeRequestKind]string{
+	raftmembership.JoinRequest:  "POST",
+	raftmembership.LeaveRequest: "DELETE",
+}
---
+++ layer_test.go
@@ -0,0 +1,295 @@
+package rafthttp_test
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/dqlite/raft-http"
+)
+
+// The Accept method receives connections from the conns channel.
+func TestLayer_Accept(t *testing.T) {
+	handler := rafthttp.NewHandler()
+	layer := rafthttp.NewLayer("/", nil, handler, nil)
+	want, _ := net.Pipe()
+	go func() {
+		handler.Connections() <- want
+	}()
+	got, err := layer.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Fatal("Accept() didn't receive the connection through the conns channel")
+	}
+}
+
+// The Accept method returns an error if the layer was closed.
+func TestLayer_AcceptWhenClosed(t *testing.T) {
+	handler := rafthttp.NewHandler()
+	layer := rafthttp.NewLayer("/", nil, handler, nil)
+	layer.Close()
+	conn, err := layer.Accept()
+	if conn != nil {
+		t.Fatal("Expected nil connection")
+	}
+	if err != io.EOF {
+		t.Fatal("Expected EOF error")
+	}
+}
+
+// The Close method is a no-op.
+func TestLayer_Close(t *testing.T) {
+	layer := rafthttp.NewLayer("/", nil, rafthttp.NewHandler(), nil)
+	if err := layer.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// The Addr method return the local address.
+func TestLayer_Addr(t *testing.T) {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	addr := listener.Addr()
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	if layer.Addr() != addr {
+		t.Fatal("Addr() did not return the configured address")
+	}
+}
+
+// If the given HTTP path is invalid, an error is returned.
+func TestLayer_DialPathParseError(t *testing.T) {
+	layer := rafthttp.NewLayer("%:not\a/path", nil, nil, nil)
+	want := "invalid URL path %:not\a/path"
+	defer func() {
+		if got := recover(); got != want {
+			t.Fatalf("Invalid path did not panic as expected: %s (should be %s)", got, want)
+		}
+	}()
+	layer.Dial(":0", time.Second)
+}
+
+// If the Dial function returns an error when trying to connect, it
+// gets propagated.
+func TestLayer_DialDialError(t *testing.T) {
+	dial := func(string, time.Duration) (net.Conn, error) {
+		return nil, fmt.Errorf("connection error")
+	}
+	layer := rafthttp.NewLayer("/", nil, nil, dial)
+	conn, err := layer.Dial(":0", time.Second)
+	if conn != nil {
+		t.Fatal("expected nil conn")
+	}
+	if err == nil {
+		t.Fatal("expected error")
+	}
+	const want = "dialing failed: connection error"
+	got := err.Error()
+	if want != got {
+		t.Fatalf("expected error\n%q\ngot\n%q", want, got)
+	}
+
+}
+
+// If an error occurs while writing the HTTP request, it is returned.
+func TestLayer_DialRequestWriteError(t *testing.T) {
+	dial := func(string, time.Duration) (net.Conn, error) {
+		conn, _ := net.Pipe()
+		conn.Close() // This will trigger a write error
+		return conn, nil
+	}
+	layer := rafthttp.NewLayer("/", nil, nil, dial)
+	conn, err := layer.Dial(":0", time.Second)
+	if conn != nil {
+		t.Fatal("expected nil conn")
+	}
+	if err == nil {
+		t.Fatal("expected error")
+	}
+	const want = "sending HTTP request failed: io: read/write on closed pipe"
+	got := err.Error()
+	if want != got {
+		t.Fatalf("expected error error\n%q\ngot\n%q", want, got)
+	}
+
+}
+
+// If the response sent by the server has not code 101, an error is
+// returned.
+func TestLayer_DialResponseWrongStatusCode(t *testing.T) {
+	server, client := net.Pipe()
+	dial := func(string, time.Duration) (net.Conn, error) {
+		return client, nil
+	}
+	layer := rafthttp.NewLayer("/", nil, nil, dial)
+	go func() {
+		// Read a line from the request data written by the
+		// layer via the client conn. This unblocks reading
+		// the response
+		buffer := bufio.NewReader(server)
+		_, _, err := buffer.ReadLine()
+		if err != nil {
+			t.Fatalf("Failed to read client request: %v", err)
+		}
+		// Send a malformed response to the client.
+		server.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	}()
+	conn, err := layer.Dial(":0", time.Second)
+	if conn != nil {
+		t.Fatal("Dial() returned a non-nil connection")
+	}
+	if err == nil {
+		t.Fatal("Dial() didn't return any error")
+	}
+	if err.Error() != "dialing fail: expected status code 101 got 200" {
+		t.Fatalf("Dial() returned unexpected error: %v", err)
+	}
+}
+
+// If an error occurs while reading the HTTP request, it is returned.
+func TestLayer_DialResponseReadError(t *testing.T) {
+	server, client := net.Pipe()
+	dial := func(string, time.Duration) (net.Conn, error) {
+		return client, nil
+	}
+	layer := rafthttp.NewLayer("/", nil, nil, dial)
+	go func() {
+		// Read a line from the request data written by the
+		// layer via the client conn. This unblocks reading
+		// the response
+		buffer := bufio.NewReader(server)
+		_, _, err := buffer.ReadLine()
+		if err != nil {
+			t.Fatalf("Failed to read client request: %v", err)
+		}
+		// Send a malformed response to the client.
+		server.Write([]byte("garbage\n"))
+	}()
+	conn, err := layer.Dial("127.0.0.1:0", time.Second)
+	if conn != nil {
+		t.Fatal("expected nil conn")
+	}
+	if err == nil {
+		t.Fatal("expected error")
+	}
+	const want = `failed to read response: malformed HTTP response "garbage"`
+	got := err.Error()
+	if want != got {
+		t.Fatalf("expected error\n%q\ngot\n%q", want, got)
+	}
+}
+
+// If the response sent by the server has not an Upgrade header set to
+// "raft", an error is returned.
+func TestLayer_DialResponseNoUpgradeHeader(t *testing.T) {
+	server, client := net.Pipe()
+	dial := func(string, time.Duration) (net.Conn, error) {
+		return client, nil
+	}
+	layer := rafthttp.NewLayer("/", nil, nil, dial)
+	go func() {
+		// Read a line from the request data written by the
+		// layer via the client conn. This unblocks reading
+		// the response
+		buffer := bufio.NewReader(server)
+		_, _, err := buffer.ReadLine()
+		if err != nil {
+			t.Fatalf("Failed to read client request: %v", err)
+		}
+		// Send a malformed response to the client.
+		server.Write([]byte("HTTP/1.1 101 Switching Protocols\r\n\r\n"))
+	}()
+	conn, err := layer.Dial(":0", time.Second)
+	if conn != nil {
+		t.Fatal("Dial() returned a non-nil connection")
+	}
+	if err == nil {
+		t.Fatal("Dial() didn't return any error")
+	}
+	if err.Error() != "missing or unexpected Upgrade header in response" {
+		t.Fatalf("Dial() returned unexpected error: %v", err)
+	}
+}
+
+// If the HTTP request to join a cluster returns an unexpected status
+// code, an error is returned.
+func TestLayer_JoinErrorStatusCode(t *testing.T) {
+	// Setup a handler that always returns 404.
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	}))
+	defer server.Close()
+
+	addr := server.Listener.Addr()
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	err := layer.Join(addr.String(), time.Second)
+	if err == nil {
+		t.Fatal("Join call did not fail")
+	}
+	if err.Error() != fmt.Sprintf("peer join failed: http code 404 '404 page not found'") {
+		t.Fatalf("Got unexpected error: %v", err)
+	}
+}
+
+// If there's a network failure while leaving, an error is returned.
+func TestLayer_LeaveNetworkError(t *testing.T) {
+	addr := &net.TCPAddr{IP: []byte{0, 0, 0, 0}, Port: 0}
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	err := layer.Leave(addr.String(), time.Second)
+	if err == nil {
+		t.Fatal("Leave call did not fail")
+	}
+}
+
+// If there's a network failure while joining, an error is returned.
+func TestLayer_JoinNetworkError(t *testing.T) {
+	addr := &net.TCPAddr{IP: []byte{0, 0, 0, 0}, Port: 0}
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	err := layer.Join(addr.String(), time.Second)
+	if err == nil {
+		t.Fatal("Join call did not fail")
+	}
+}
+
+// If the URL in the Location header of a redirect is not valid, an
+// error is returned.
+func TestLayer_JoinLocationParseError(t *testing.T) {
+	// Setup a handler that returns an invalid Location.
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, ":/%:not\a/url", rafthttp.StatusPermanentRedirect)
+	}))
+	defer server.Close()
+
+	addr := server.Listener.Addr()
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	if err := layer.Join(addr.String(), time.Second); err == nil {
+		t.Fatal("Join call did not fail")
+	}
+}
+
+func TestLayer_JoinRetryIfServiceUnavailable(t *testing.T) {
+	// Setup a handler that fails only the first time.
+	retried := false
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !retried {
+			http.Error(w, "leader unknown", http.StatusServiceUnavailable)
+			retried = true
+		}
+	}))
+	defer server.Close()
+
+	addr := server.Listener.Addr()
+	layer := rafthttp.NewLayer("/", addr, nil, nil)
+	if err := layer.Join(addr.String(), time.Second); err != nil {
+		t.Fatalf("Join request failed although it was supposed to retry: %v", err)
+	}
+}
